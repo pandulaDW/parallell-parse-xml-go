c34134be4dcdb47bf2df7d8efad415ddbfc529ea
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2020-12-05 14:19:33 +0530

    Finished parsing the 3.8GB file

--- main.go
+++ main.go
@@ -1,39 +1,54 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
-	"strings"
+	"log"
+	"os"
 	"time"
 )
 
 func concurrent() {
 	start := time.Now()
 	ch := make(chan string)
-	bufferedReader, err := createBufferedReader("data/20201203-gleif-concatenated-file-rr.xml.5fc8c1302bde7.zip")
+
+	// bufferedReader, err := createBufferedReader("data/20201203-gleif-concatenated-file-rr.xml.5fc8c1302bde7.zip")
+	inFile, err := os.Open("data/20201202-gleif-concatenated-file-lei2.xml")
 	if err != nil {
 		panic(err)
 	}
+	defer inFile.Close()
 
-	recordSets := readAndUnmarshalByStream(bufferedReader, 500, ch)
+	bufferedReader := bufio.NewReader(inFile)
+	recordSets := readAndUnmarshalByStreamLEI(bufferedReader, 1000, ch)
 
-	sb := strings.Builder{}
+	outFile, err := os.OpenFile("data/sampleWrite.csv", os.O_CREATE|os.O_RDWR, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer outFile.Close()
+
+	writer := bufio.NewWriter(outFile)
 	count := 0
 
-	header := createCsvHeader()
-	sb.WriteString(header)
-	sb.WriteByte('\n')
+	// header := createCsvHeader()
+	// sb.WriteString(header)
+	// sb.WriteByte('\n')
 
 	for count < recordSets {
 		recordSet := <-ch
-		sb.WriteString(recordSet)
+		writer.WriteString(recordSet)
 		count++
+
+		if count%50 == 0 {
+			writer.Flush()
+		}
 	}
 
-	ioutil.WriteFile("data/sampleWrite.csv", []byte(sb.String()), 0666)
+	writer.Flush()
 	fmt.Printf("%d concurrent parses with time taken: %v", recordSets, time.Since(start))
 }
 
 func main() {
-	test()
+	concurrent()
 }
